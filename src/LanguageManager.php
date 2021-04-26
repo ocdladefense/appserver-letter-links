4f182bb269f22b278a601ce7e11f2a790a8593d9
unknown <betettag@my.lanecc.edu>
2021-04-26 16:49:17 -0700

    adding base files

---
+++ src/LanguageManager.php
@@ -0,0 +1,18 @@
+<?php
+
+class LanguageManager {
+
+
+    // public function getPicturePacks($sound,$letter = null){
+        
+    // }
+    public function __construct($lang)
+	{
+
+    }
+
+    public static function setLanguage($lang){
+        //return lang class
+    }
+}
+?>
---
+++ src/SoundManager.php
@@ -0,0 +1,15 @@
+<?php
+
+class SoundManager {
+    private $sounds; //list of option tags
+
+    public static function getSoundPacks($lang,$letter = null){
+                //get sounds by query
+    }
+
+    public static function getSound($lang,$letter,$sound){
+                //get sounds by query
+    }
+
+}
+?>
