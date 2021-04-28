d34018f0414e085e798c59ca0c5af3c08beddf9c
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2021-04-28 12:08:44 -0700

    Fix some css, additional clean up.

--- templates/pictures.tpl.php
+++ templates/pictures.tpl.php
@@ -3,9 +3,9 @@
 
 <div id="picture-list" class="picture-list">
     <?php foreach($pictures as $caption => $url) : ?>
-        <div id="picture-item" class="picture-item" style="display: inline;">
+        <div id="picture-item" class="picture-item" style="display: inline-block; text-align: center;">
             <label><?php print $caption; ?></label>
-            <img src="<?php print $url; ?>" alt="This image file would not load." />
+            <img src="<?php print $url; ?>" style="display: block;" alt="This image file would not load." />
         </div>
     <?php endforeach ?>
 </div>
