ee9c2cce5a422ad92e0c4a66d33ee9f73d63e3c6
unknown <ginobetetta@gmail.com>
2021-04-29 12:16:16 -0700

    consolidated all routes into a single route

--- module.php
+++ module.php
@@ -8,97 +8,23 @@ class LetterLinksModule extends Module
 
 	public function __construct()
 	{
-		parent::__construct($path);
+		parent::__construct();
 	}
 
-	public function translate(){
-		$lang = $_GET["lang"] ?? "sp";
-		$tpl = new Template("translate");
+	public function showPage($page){
+		$loadFromFile = true;
+		$language = null;
+		$content = t($page,$language,$loadFromFile);
+
+		$tpl = new Template("page");
+
 		$tpl->addPath(__DIR__ . "/templates");
-		return $tpl->render(array(		));
+
+
+		return $tpl->render(array(
+			"content" => $content
+		));
 	}
-
-	public function es($route){
-		switch ($route) {
-			case 'faq':
-				return $this->faq("es");
-				break;
-			case 'about':
-				return $this->about("es");
-				break;
-			case 'help':
-				return $this->help("es");
-				break;
-			case 'resources':
-				return $this->resources("es");
-				break;
-			case 'activities':
-				return $this->activities("es");
-				break;
-			default:
-				$this->about("es");
-				break;
-		}
-	}
-
-	public function about($lang = null)
-	{
-		$lang = $_GET["lang"] ?? "sp";
-		$tpl = new TestTemplate("letter-links-".$lang);
-		$tpl->addPath(__DIR__ . "/templates");
-		return $tpl->render(array(		));
-	}
-
-	public function faq($lang = null)
-	{
-		$homeFiles = __DIR__ . '/src/faq';
-		$lang = $lang == "es" ? "-".$lang : "";
-        $tpl = new Template("faq".$lang);
-		$tpl->addPath(__DIR__ . "/templates");
-		//$tpl->addStyles($homeFiles."/styles");
-		//$tpl->addScripts($homeFiles."/scripts");
-        return $tpl->render(array(
-			"files" => $homeFiles
-		));
-    }
-
-
-	public function help($lang = null)
-	{
-		$homeFiles = __DIR__ . '/src/help';
-		$lang = $lang == "es" ? "-".$lang : "";
-        $tpl = new Template("help".$lang);
-		$tpl->addPath(__DIR__ . "/templates");
-		//$tpl->addStyles($homeFiles."/styles");
-		//$tpl->addScripts($homeFiles."/scripts");
-        return $tpl->render(array(
-			"files" => $homeFiles
-		));
-    }
-	public function resources($lang = null)
-	{
-		$homeFiles = __DIR__ . '/src/resources';
-		$lang = $lang == "es" ? "-".$lang : "";
-        $tpl = new Template("resources".$lang);
-		$tpl->addPath(__DIR__ . "/templates");
-		//$tpl->addStyles($homeFiles."/styles");
-		//$tpl->addScripts($homeFiles."/scripts");
-        return $tpl->render(array(
-			"files" => $homeFiles
-		));
-    }
-	public function activities($lang = null)
-	{
-		$homeFiles = __DIR__ . '/src/activities';
-		$lang = $lang == "es" ? "-".$lang : "";
-        $tpl = new Template("activities".$lang);
-		$tpl->addPath(__DIR__ . "/templates");
-		//$tpl->addStyles($homeFiles."/styles");
-		//$tpl->addScripts($homeFiles."/scripts");
-        return $tpl->render(array(
-			"files" => $homeFiles
-		));
-    }
 
 	public function login()
 	{
@@ -111,24 +37,6 @@ class LetterLinksModule extends Module
 			"files" => $homeFiles
 		));
     }
-
-
-
-
-	public function test(){
-        $t = new T(__DIR__ . '/src/about',array("en.txt","es.txt"));
-
-		// $tpl = new TestTemplate("letter-links-en");
-		$lang = $_GET["lang"] ?? "sp";
-		
-		$tpl = new TestTemplate("letter-links-".$lang);
-		
-		
-		$tpl->addPath(__DIR__ . "/templates");
-        return $tpl->render(array(
-			"t" => $t
-		));
-	}
 
 	///////////////////////////	PICTURE MANAGER FUNCTIONALITY	//////////////////////////////////////////////////////
 
