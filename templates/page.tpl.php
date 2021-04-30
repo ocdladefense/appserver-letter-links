f6cf9c4066ed366269f34912c132bf82d2578c1c
unknown <ginobetetta@gmail.com>
2021-04-30 09:38:54 -0700

    mobile compatability and fixing css

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -1,12 +1,9 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <style>
-    body {
-
-    }
 
     .sidenav {
         height: 100%;
-        width: 0;
+        width: 20%;
         position: fixed;
         z-index: 1;
         top: 30px;
@@ -43,7 +40,8 @@
     #main {
         /*transition: margin-left .5s;*/
         padding: 16px;
-        padding-bottom:27px;
+        padding-bottom:127px;
+        margin-left: 20%;
     }
 
     @media screen and (max-height: 450px) {
@@ -67,10 +65,35 @@
         background-color: #00a8e7;
         color: white;
         z-index: 2;
-        text-decoration: none;
         font-size: 17px;
         text-align: center;
-        min-height: 200px;
+        min-height: 100px;
+        padding-top: 20px;
+    }
+    .footer a{
+        text-decoration: none !important;
+        color: white !important;
+        text-transform: uppercase;
+        letter-spacing: -1px;
+
+
+    }
+
+    p{
+        padding-top: 10px;
+        padding-bottom: 10px;
+    }
+    ol{
+        padding: 10px;
+    }
+    ul{
+        padding: 10px;
+    }
+    .title {
+        font-size: 14pt;
+        text-transform: uppercase;
+        font-weight: 700;
+        color: #fc4c02;
     }
     /* changes? */
     #stage-content{
@@ -93,7 +116,7 @@
     }
     .dropdown-container {
         display: none;
-        background-color: #rgb(51 75 179);
+        background-color: rgb(51 75 179);
         padding-left: 8px;
     }
 
@@ -101,6 +124,48 @@
     @media screen and (max-height: 450px) {
         .sidenav {padding-top: 15px;}
         .sidenav a {font-size: 18px;}
+    }
+    @media only screen and (min-width: 700px) {
+        #openbtn{display:none;}
+        .sidenav{width:20%}
+        #main{margin-left:20%}
+        
+    }
+
+    @media only screen and (max-width: 959px) {
+        #title{font-size: 27pt;}
+        #main{font-size: larger;}
+        
+    }
+    @media only screen and (max-width: 700px) {
+        #title{font-size: 23pt;}
+            .sidenav a{font-size: 12pt; 
+            padding-top:15px;
+            padding-left:20px;
+        }
+    }
+    @media only screen and (max-width: 613px) {
+
+            .sidenav {width: 0}
+            #main   {margin-left:0 !important;}
+            .closebtn {
+                display:unset !important;
+                top: 37px !important;
+                right: 9px !important;
+            }
+            #openbtn    {
+                background: #012169;
+                color: white;
+                padding: 11px 15px;
+                border-radius: 25px;
+                box-shadow: 0px 17px 10px -10px rgba(0,0,0,0.4);
+                display:unset !important;
+                transition: all ease-in-out 300ms;
+                position: fixed;
+                top: 56px;
+                z-index: 11;
+                right: 10px;
+            }
     }
 </style>
 </head>
@@ -124,26 +189,31 @@
   </div>
 </div>
 
+<div id="openbtn" style="display:none;">
+    <span style="font-size:30px; cursor:pointer" onclick="openNav('40%')">&#9776; </span>
+</div>
+
 <div id="main">
-  <span  style="display:none; font-size:30px; cursor:pointer" onclick="openNav()">&#9776; open</span>
   <?php echo($content); ?>
 </div>
 <div class="footer">
-    <a href="terms"><?php echo(t("footer_terms"));?></a>&nbsp;&nbsp;
-    <a href="contact"><?php echo(t("footer_contact"));?></a>&nbsp;&nbsp;
-    <a href="policy"><?php echo(t("footer_policy"));?></a>&nbsp;&nbsp;
-    <a href="#"><?php echo(t("footer_store"));?></a>&nbsp;&nbsp;
+    <a href="terms"><?php echo(t("footer_terms"));?></a>&nbsp;
+    <a href="contact"><?php echo(t("footer_contact"));?></a>&nbsp;
+    <a href="policy"><?php echo(t("footer_policy"));?></a>&nbsp;
+    <a href="#"><?php echo(t("footer_store"));?></a>&nbsp;
 </div>
 <script>
 
-function openNav() {
-  document.getElementById("mySidenav").style.width = "20%";
-  document.getElementById("main").style.marginLeft = "20%";
+function openNav(amount = "20%") {
+  document.getElementById("mySidenav").style.width = amount;
+  document.getElementById("main").style.marginLeft = amount;
+  document.getElementById("openbtn").style.display="none !important";  
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
   document.getElementById("main").style.marginLeft= "0";
+  document.getElementById("openbtn").style.display="unset"; 
 }
 
 var dropdown = document.getElementsByClassName("dropdown-btn");
@@ -160,8 +230,6 @@ for (i = 0; i < dropdown.length; i++) {
   }
   });
 }
-
-window.onload(openNav());
 </script>
 
 
