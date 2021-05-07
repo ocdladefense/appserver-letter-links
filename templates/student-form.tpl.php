7666853fb2a0f8bf5eb7badfa554a72bb162ac9b
unknown <ginobetetta@gmail.com>
2021-05-06 19:29:03 -0700

    standalone student view final and starting work for student list

--- templates/student-form.tpl.php
+++ templates/student-form.tpl.php
@@ -2,17 +2,17 @@
 	function editName(){
 		document.getElementById("editName").style.display='block';
 		document.getElementById("editStudent").style.display='none';
-		document.getElementById("pictureBtn").style.display='none';
 		document.getElementById("cancelStudent").style.display='inline-block';
 		document.getElementById("studentForm").style.display='unset';
+		document.getElementById("lang").style.display='none';
 		
 	}
 	function cancel(){
 		document.getElementById("editName").style.display='none';
 		document.getElementById("editStudent").style.display='unset';
-		document.getElementById("pictureBtn").style.display='block';
 		document.getElementById("cancelStudent").style.display='none';
 		document.getElementById("studentForm").style.display='none';
+		document.getElementById("lang").style.display='unset';
 	}
 </script>
 
@@ -36,20 +36,18 @@
 
 <div class="card-container">
     <div class="top-label">
-		<span class="student"><i class="fas fa-apple-alt"></i> Student</span>
+		<i class="student fas fa-apple-alt"></i>
+		<span class="delete"><a href="#"><i style="font-size: x-large;" class="fas fa-trash-alt"></i><span name="descriptions">delete</span></a></span>
 	</div>
 	<div class="nameTitle">
 		<h1 style="display: inline-block;"><?php print $student->getName(); ?> </h1>
-		<div id="editStudent" onclick="editName();"><i style="font-size: x-large;" class="fas fa-pencil-alt" ></i>edit</div>
-		<div id="cancelStudent" onclick="cancel();"><i  style="font-size: x-large;" class="fa fa-times" aria-hidden="true"></i>cancel</div>
+		<div id="editStudent" onclick="editName();"><i style="font-size: x-large;" class="fas fa-pencil-alt" ></i><span name="descriptions">Edit Student</span></div>
+		<div id="cancelStudent" onclick="cancel();"><i  style="font-size: x-large;" class="fa fa-times" aria-hidden="true"></i><span name="descriptions">cancel</span></div>
+		<br/><h4 style="display: inline-block;"><?php print $student->getLetterLinkCaption(); ?></h4>
+		<div id="editPicture"><i style="font-size: x-large; display: inline-block;" class="fas fa-image"></i><span name="descriptions">Edit Picture</span></div>
 	</div>
 	<div style="width: 100%;">
 		<img src="<?php print $student->getLetterLinkImageUrl(); ?>" alt="student-img">
-		<div class="buttons" id="pictureBtn">
-			<button class="primary ghost">
-				<i class="fas fa-image"></i> Change
-			</button>
-		</div>
 	</div>
     
 
@@ -58,10 +56,12 @@
 		<input type="text" class="name" name="name" id="name" value="<?php print $student->getName(); ?>" placeholder="Change Name Here:" />
 	</div>
 
-	<label for="Name" style="padding-top: 10px; padding-bottom: 10px; text-transform: capitalize;"><?php print t("language")?>: <?php print "default language"?></label>
+	<label id="lang" for="Name" style="padding-top: 10px; padding-bottom: 10px; text-transform: capitalize;"><?php print t("language")?>: <?php print "default language"?></label>
 	<div id="studentForm" style="display: none; padding:10px 0px 10px 0px;">
-	</br>
+		<label for="Name" style="padding-top: 10px; padding-bottom: 10px; text-transform: capitalize;"><?php print t("language")?>:</label>
+		</br>
 		<div class="toggle-radio">
+			
 			<input type="radio" name="default" id="default_Option1" value="Option1" checked>
 			<label for="default_Option1"><?php print "default language"?></label>
 			<input type="radio" name="default" id="default_Option2" value="Option2" >
@@ -78,19 +78,6 @@
 			</button> -->
 		</div>
 	</div>
-
-
-
-
-    <!-- <p>User interface designer and <br> front-end developer</p>
-    <div class="buttons">
-        <button class="primary">
-            Message
-        </button>
-        <button class="primary ghost">
-            Following
-        </button>
-    </div> -->
     <div class="skills">
         <h5>Classes:</h5>
         <ul style="font-size: 13pt;">
