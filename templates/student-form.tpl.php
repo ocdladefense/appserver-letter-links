a2490eb5ac9586df087857924be0144862c38bae
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2021-05-17 10:49:59 -0700

    A link to take you back to the student list.

--- templates/student-form.tpl.php
+++ templates/student-form.tpl.php
@@ -17,7 +17,7 @@
 	}
 </script>
 
-
+<a href="/classes/<?php print $student->getClass__c(); ?>/students">back to list</a>
 
 <form enctype="multipart/form-data" id="upload-form" class="card-container" name="upload-form" method="post" action="/update/student" >
 
